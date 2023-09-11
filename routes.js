b3b4fdeb6f7854eb3d800dbc01823fb12e825211
Jonathan Göransson <jonathan.a.goransson@outlook.com>
2023-09-11 17:09:14 +0200

    added a login page

--- routes.js
+++ routes.js
@@ -7,10 +7,26 @@ const router = express.Router();
 router.get("/", (req, res) => {
     let data = {
         title: "Index",
-        today: new Date()
+        today: new Date(),
     };
 
     res.render("index.ejs", data);
+});
+
+router.get("/login", (req, res) => {
+    let data = {
+        title: "Pulse | Login",
+    };
+
+    res.render("login.ejs", data);
+});
+
+router.post("/login", (req, res) => {
+    let data = req.params;
+
+    console.log(data);
+
+    res.redirect("/");
 });
 
 module.exports = router;
